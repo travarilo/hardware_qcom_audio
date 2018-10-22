a24d54fd386b296f277ed956925f4b69ab5fae93
Rashed Abdel-Tawab <rashed@linux.com>
2018-10-21 21:43:18 -0700

    hal: Fix forgotten guards
    Change-Id: I471b5b5901e0e190133e94cd7462f42a4730e9c5

--- hal/audio_hw.c
+++ hal/audio_hw.c
@@ -2967,9 +2967,11 @@ static int stop_output_stream(struct stream_out *out)
 
         if (adev->offload_effects_stop_output != NULL)
             adev->offload_effects_stop_output(out->handle, out->pcm_device_id);
+#ifdef PERF_HINTS_ENABLED
     } else if (out->usecase == USECASE_AUDIO_PLAYBACK_ULL ||
                out->usecase == USECASE_AUDIO_PLAYBACK_MMAP) {
         audio_low_latency_hint_end();
+#endif
     }
 
     if (out->usecase == USECASE_INCALL_MUSIC_UPLINK)
@@ -3299,10 +3301,12 @@ int start_output_stream(struct stream_out *out)
 #endif
     audio_extn_perf_lock_release(&adev->perf_lock_handle);
 
+#ifdef PERF_HINTS_ENABLED
     if (out->usecase == USECASE_AUDIO_PLAYBACK_ULL ||
         out->usecase == USECASE_AUDIO_PLAYBACK_MMAP) {
         audio_low_latency_hint_start();
     }
+#endif
 
     ALOGD("%s: exit", __func__);
 
