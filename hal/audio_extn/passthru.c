ecedd3467f097220daec3138b216e4196f7c1138
Dede Dindin Qudsy <xtrymind@gmail.com>
2019-08-03 13:54:02 +0700

    treewide: replace cutils/log.h to log/log.h
    * only replace affected file when building X00T.
    
    system/core/libcutils/include_vndk/cutils/log.h:38:2: warning: "Deprecated: don't include cutils/log.h, use either android/log.h or log/log.h" [-W#warnings]
    warning "Deprecated: don't include cutils/log.h, use either android/log.h or log/log.h"
    
    Signed-off-by: Dede Dindin Qudsy <xtrymind@gmail.com>

--- hal/audio_extn/passthru.c
+++ hal/audio_extn/passthru.c
@@ -32,7 +32,7 @@
 #include <stdlib.h>
 #include <cutils/atomic.h>
 #include <cutils/str_parms.h>
-#include <cutils/log.h>
+#include <log/log.h>
 #include <unistd.h>
 #include <pthread.h>
 #include "audio_hw.h"
